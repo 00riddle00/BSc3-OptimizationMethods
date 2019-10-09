6777a2215b9503277263b612ac73a3b7ca428aaf
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-10-09 15:14:07 +0300

    .

--- lab2/GradientDescent.m
+++ lab2/GradientDescent.m
@@ -1,12 +1,19 @@
-function NiutonoMetodas
-% Niutono metodu randamas funkcijos f(x) minimumas intervale [l,r].
+function GradientDescent
 
-f=@(x)((x.^2-4).^2)/7-1;
-f1=@(x)4*x*(x.^2-4)/7;
-f2=@(x)4*(3*x.^2-4)/7;
+f=@(x1, x2)(1/8)*((x1.^2).*x2 + x1.*(x2.^2) - x1.*x2);
 
-l=0;   % apatinis intervalo rezis
-r=10;  % desinysis intervalo rezis
+% ieskome min(f(x1, x2))
+
+gradf=@(X) [2*X(1)*X(2) + X(2).^2 - X(2), 
+            X(1)^2 + 2*X(1)*X(2) - X(1)];
+
+X0 = [1/2,1/2];
+#X0 = [0,0]
+%X1 = [1,1]
+%Xm = [4/10, 7/10]
+
+%gamma_reziai = [0,1]
+gamma = 1/2;
 
 epsilon=10^(-4); %tikslumas
 
@@ -14,33 +21,31 @@ k=1; %iteraciju skaitliukas
 kmax=100; % maksimalus iteraciju skaitliukas
 
 %Funkcijos grafiko y=f(x) braizymas
-x=l:0.01:r;
-y=f(x);
-plot(x,y,'b');
-grid on;
-xlabel('x asis');
-ylabel('y asis');
-title(['Funkcijos y=f(x) grafikas ir artiniai']);
+%x=l:0.01:r;
+%y=f(x);
+%plot(x,y,'b');
+%grid on;
+%xlabel('x asis');
+%ylabel('y asis');
+%title(['Funkcijos y=f(x) grafikas ir artiniai']);
 
 %Metodo realizavimas
 
-x0=5;
-delta=1;
-disp(['     x1        y1        k         funkc. kviet. sk']);
-
+%x0=5;
+%delta=1;
+disp(['     x1        x2        f(x1,x2)       k         funkc. kviet. sk']);
+   
 format long
 
-while delta>=epsilon
-      x1=x0-f1(x0)/f2(x0);
-      y1=f(x1);
-      delta=abs(x1-x0);
-      x0=x1;
+norma = Inf;
 
-      format long
-      disp([x1, y1]);
-      format short
-      disp([k, 2*k]);
-                 
+while norma >=epsilon
+  
+      grad = gradf(X0);
+      X1 = X0 - gamma * grad;
+      norma = norm(grad)
+ 
+      fprintf('%f %f %f %d %d\n', X1, f( X1(1), X1(2) ), k, k);             
       hold on
       plot(x1, y1, 'ro')
       
@@ -50,5 +55,6 @@ while delta>=epsilon
           break
       end 
       k=k+1;
+      X0 = X1;
 end
 end
