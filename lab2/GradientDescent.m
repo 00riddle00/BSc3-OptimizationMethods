53475cd412274370a7bbe8cdc045c9ec1acc12f3
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-10-09 20:16:57 +0300

    .

--- lab2/GradientDescent.m
+++ lab2/GradientDescent.m
@@ -1,22 +1,27 @@
 function GradientDescent
 
-f=@(x1, x2)(1/8)*((x1.^2).*x2 + x1.*(x2.^2) - x1.*x2);
-
+f=@(x1, x2) (1/8)*((x1.^2).*x2 + x1.*(x2.^2) - x1.*x2);
 % ieskome min(f(x1, x2))
 
 gradf=@(X) [2*X(1)*X(2) + X(2).^2 - X(2), X(1)^2 + 2*X(1)*X(2) - X(1)];       
-            
-%X0 = [1/2,1/2];
-%X0 = [0,0]
-X0 = [1,1]
-%Xm = [4/10, 7/10]
 
+% pradiniai artinai
+X_0 = [0,0]
+X_1 = [1,1]
+X_m = [4/10, 7/10]
+X_n = [1/2,1/2];
+
+% pasirenkamas pradinis artinys
+X0 = X_m
+
+%Funkcijos grafiko y=f(x1,x2) braizymas
 subplot(1,2,1)
 [x1,x2] = meshgrid(0:0.01:1, 0:0.01:1);
 y = f(x1, x2)
 surf(x1, x2, y);
+title(['Funkcijos y=f(x1,x2) grafikas'])
 
-%gamma_reziai = [0,1]
+% pasirenkamas parametras
 gamma = 1;
 
 epsilon=10^(-4); %tikslumas
@@ -24,20 +29,9 @@ epsilon=10^(-4); %tikslumas
 k=1; %iteraciju skaitliukas
 kmax=100; % maksimalus iteraciju skaitliukas
 
-%Funkcijos grafiko y=f(x) braizymas
-%x=l:0.01:r;
-%y=f(x);
-%plot(x,y,'b');
-%grid on;
-%xlabel('x asis');
-%ylabel('y asis');
-%title(['Funkcijos y=f(x) grafikas ir artiniai']);
-
 %Metodo realizavimas
 
-%x0=5;
-%delta=1;
-disp(['     x1        x2        f(x1,x2)       k         funkc. kviet. sk']);
+disp(['x1       x2       f(x1,x2)  k (funkc. kviet. sk)']);
    
 format long
 
@@ -46,13 +40,16 @@ norma = Inf;
 while norma >= epsilon
   
       grad = gradf(X0);
-      X0 = X0 - gamma.*grad;
+      X0 = X0 - gamma.*grad; % naujas artinys 
       norma = norm(grad);
  
       fprintf('%f %f %f %d %d\n', X0, f( X0(1), X0(2) ), k, k);             
       
       subplot(1,2,2);
-      plot(X0(1), X0(2), 'o'); % X1 -naujas artinys 
+      title(['Artiniai'])
+      %fig = gcf;
+      %fig.Color = [1 0.4 0.6];
+      plot(X0(1), X0(2), 'mo');
       hold on;
       
       if k==kmax
@@ -64,5 +61,5 @@ while norma >= epsilon
 
 end
 grid on;
-hold off;
+hold off;    
 end
