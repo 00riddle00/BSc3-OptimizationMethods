fec3a82f8427a2fdf336b1081d154589fab3e60d
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-10-18 22:51:15 +0300

    .

--- lab2/SteepestDescent.m
+++ lab2/SteepestDescent.m
@@ -9,21 +9,10 @@ gradf = @(X) [2 * X(1) * X(2) + X(2) .^ 2 - X(2), X(1) ^ 2 + 2 * X(1) * X(2) - X
 X_0 = [0, 0];
 X_1 = [1, 1];
 X_m = [4 / 10, 7 / 10];
+X_n = [1 / 2, 1 / 2];
 
 % pasirenkamas pradinis artinys
-X0 = X_1;
-
-% Funkcijos grafiko y=f(x1,x2) braizymas
-subplot(1, 2, 1);
-[x1, x2] = meshgrid(0:0.01:1, 0:0.01:1);
-y = f(x1, x2);
-surf(x1, x2, y);
-title(['Funkcijos y=f(x1,x2) grafikas']);
-
-% pasirenkamas iteracijos parametras (kokio ilgio zingsnis atliekamas gradiento kryptimi)
-% gamma reziai: (0;2/L), kur L - Lipsico konstanta.
-% Testuosime 0 < gamma < 2
-gamma = 0.33; % 
+X0 = X_n;
 
 epsilon = 10 ^ (- 4); % tikslumas
 
@@ -41,10 +30,12 @@ norma = Inf;
 while norma >= epsilon
  
     grad = gradf(X0);
+    res=AuksinisPjuvis(f,X0,grad);
+    gamma=res(1,1)
     X0 = X0 - gamma .* grad; % naujas artinys
     norma = norm(grad);
  
-     fprintf('%f %f %f %d %d\n', X0, f(X0(1), X0(2)), k, k);
+    fprintf('%f %f %f %d %d\n', X0, f(X0(1), X0(2)), k, k);
  
     subplot(1, 2, 2);
     title(['Artiniai (pradinis taškas: X1)']);
---
+++ labx/AuksinisPjuvis.m
@@ -0,0 +1,42 @@
+function ats=AuksinisPjuvis(f, X0, grad)
+
+f1=@(x) f(X0(1,1)-x.*grad(1,1), X0(1,2)-x.*grad(1,2));
+l=0;   % apatinis intervalo rezis
+r=5;  % desinysis intervalo rezis
+
+epsilon=10^(-4); %tikslumas
+
+k=1; %iteraciju skaitliukas
+kmax=100; % maksimalus iteraciju skaitliukas
+
+%Metodo realizavimas
+L=r-l;      %intervalo ilgis
+t=(sqrt(5)-1)/2;
+x1=r-t*L; y1=f1(x1);
+x2=l+t*L; y2=f1(x2);
+
+format short
+
+while L>= epsilon
+      if y2<y1
+          l=x1;
+          L=r-l;
+          x1=x2;
+          y1=y2;
+          x2=l+t*L;
+          y2=f1(x2);
+      else
+          r=x2;
+          L=r-l;
+          x2=x1;
+          y2=y1;
+          x1=r-t*L;
+          y1=f1(x1);
+      end
+      if k==kmax
+          break
+      end 
+      k=k+1;
+end
+ats=[x1, k+2];
+end
