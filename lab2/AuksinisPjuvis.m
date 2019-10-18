b529ac3d71bc11577a9e3c0c0e18950f56197284
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-10-18 23:56:57 +0300

    .

--- lab2/AuksinisPjuvis.m
+++ lab2/AuksinisPjuvis.m
@@ -1,6 +1,9 @@
 function res=AuksinisPjuvis(f, X0, grad)
 
 f1=@(x) f(X0-x*grad);
+
+% Auksinio pjuvio metodu randamas funkcijos f(x) minimumas intervale [l,r].
+
 l=0;   % apatinis intervalo rezis
 r=5;  % desinysis intervalo rezis
 
@@ -10,33 +13,42 @@ k=1; %iteraciju skaitliukas
 kmax=100; % maksimalus iteraciju skaitliukas
 
 %Metodo realizavimas
-L=r-l;      %intervalo ilgis
-t=(sqrt(5)-1)/2;
-x1=r-t*L; y1=f1(x1);
-x2=l+t*L; y2=f1(x2);
+gR = (sqrt(5) - 1) / 2;
 
-format short
+L = r-l; % intervalo ilgis
+x1 = r-gR*L;
+y1 = f1(x1);
+x2 = l + gR*L;
+y2 = f1(x2);
+format short;
 
 while L>= epsilon
-      if y2<y1
-          l=x1;
-          L=r-l;
-          x1=x2;
+  
+      if y2 < y1
+          l = x1;
+          L = r - l;
+          x1 = x2;
           y1=y2;
-          x2=l+t*L;
-          y2=f1(x2);
+          x2 = l + gR*L;
+          y2 = f1(x2);
       else
-          r=x2;
-          L=r-l;
-          x2=x1;
+          r = x2;
+          L = r - l;
+          x2 = x1;
           y2=y1;
-          x1=r-t*L;
-          y1=f1(x1);
-      end
-      if k==kmax
-          break
+          x1 = r - gR*L;
+          y1 = f1(x1);
       end 
+          
+      if k==kmax
+          format short
+          disp(['Pasiektas maksimalus iteraciju skaicius k=', num2str(kmax)]);
+          break
+      end   
+      
       k=k+1;
+      L=r-l;
+
 end
 res=[x1, k+2];
 end
